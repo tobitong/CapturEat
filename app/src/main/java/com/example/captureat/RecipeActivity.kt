718cc98d0e4b382ef57b6417199400763054ec90
tobitong <tobyfuady.17201@gmail.com>
2022-06-08 15:41:12 +0700

    Creating recipe detail page

--- app/src/main/java/com/example/captureat/RecipeActivity.kt
+++ app/src/main/java/com/example/captureat/RecipeActivity.kt
@@ -2,10 +2,23 @@ package com.example.captureat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.captureat.databinding.ActivityRecipeBinding
 
 class RecipeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRecipeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_recipe)
+        binding = ActivityRecipeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val recipeName = intent.getStringExtra("foodName")
+        val recipePict = intent.getIntExtra("foodPict", R.drawable.bakso)
+        val recipeIngredients = intent.getStringExtra("foodIngredients")
+
+        binding.recipeName.text = recipeName
+        binding.recipePict.setImageResource(recipePict)
+        binding.recipeIngredient.text = recipeIngredients
     }
 }
