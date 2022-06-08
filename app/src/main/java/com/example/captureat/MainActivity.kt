27a9c917425c451c4337f9f4a0414122f3336cc2
tobitong <tobyfuady.17201@gmail.com>
2022-06-08 13:25:53 +0700

    Creating new activity for recipe and adding functionality for camera button (still not working)

--- app/src/main/java/com/example/captureat/MainActivity.kt
+++ app/src/main/java/com/example/captureat/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.example.captureat
 
+import android.content.Intent
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private var our_request_code : Int = 123
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.btn_open_cam).setOnClickListener {
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            if(intent.resolveActivity(packageManager) != null) {
+                startActivityForResult(intent, our_request_code)
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == our_request_code && resultCode == RESULT_OK) {
+            val bitmap = data?.extras?.get("data") as Bitmap
+        }
     }
 }
