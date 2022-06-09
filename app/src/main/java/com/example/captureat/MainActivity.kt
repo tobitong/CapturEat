74f5e4f9f365ea11699cb771c8e6131dac3ab41e
tobitong <tobyfuady.17201@gmail.com>
2022-06-09 16:11:24 +0700

    Fixing image upload from gallery funcionality

--- app/src/main/java/com/example/captureat/MainActivity.kt
+++ app/src/main/java/com/example/captureat/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.captureat
 
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -11,16 +12,32 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private val REQUEST_CODE_GALLERY = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.btnOpenCam.setOnClickListener { openCam() }
+        binding.btnOpenGallery.setOnClickListener { openGal() }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode == Activity.RESULT_OK && requestCode == REQUEST_CODE_GALLERY) {
+            binding.ivGal.setImageURI(data?.data)
+        }
     }
 
     private fun openCam() {
-        intent = Intent(this, CameraActivity::class.java)
-        startActivity(intent)
+        val intent_cam = Intent(this, CameraActivity::class.java)
+        startActivity(intent_cam)
+    }
+
+    private fun openGal() {
+        val intent_gal = Intent(Intent.ACTION_PICK)
+        intent_gal.type = "image/*"
+        startActivityForResult(intent_gal, REQUEST_CODE_GALLERY)
     }
 }
